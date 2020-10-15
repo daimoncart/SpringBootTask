c2b1758f48854511691dd839a20a3d5358c0b859
daimoncart <raimonds.liepins@gmail.com>
2020-10-15 11:46:44 +0300

    Add Employee entity

--- src/main/java/test/boot/spring/entity/Employee.java
+++ src/main/java/test/boot/spring/entity/Employee.java
@@ -1,7 +1,28 @@
 package test.boot.spring.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
 
-@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Employee {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable=false, length=50)
+    private String firstName;
+
+    @Column(nullable=false, length=50)
+    private String lastName;
+
+    @Column(nullable=false, length=100)
+    private String email;
+
 }
