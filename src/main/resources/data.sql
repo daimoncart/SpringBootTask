391128380ef55520c23fb1395b0c20ebff53d664
daimoncart <raimonds.liepins@gmail.com>
2020-10-22 11:38:22 +0300

    Add town data insert into H2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,12 @@
-INSERT INTO employee (id, first_name, last_name, email) VALUES
-    (1, 'Ilse', 'Muusi', 'ilse.muusi@towermaster.com'),
-    (2, 'Robert', 'Arent', 'robert.arent@towermaster.com'),
-    (3, 'Marcel', 'Baader', 'm.baader@towermaster.com'),
-    (4, 'Eva', 'Zielinska', 'eva@towermaster.com'),
-    (5, 'Caspar', 'Ollson', 'caspar@towermaster.com');
+INSERT INTO town (id, name, min_visibility, max_wind) VALUES
+    (1, 'Grozny', 10000, 1.0),
+    (2, 'Valmiera', 10000, 0.5),
+    (3, 'Piza', 10000, 2.0);
+
+INSERT INTO employee (id, first_name, last_name, email, town_id) VALUES
+    (1, 'Ilse', 'Muusi', 'ilse.muusi@towermaster.com', 1),
+    (2, 'Robert', 'Arent', 'robert.arent@towermaster.com', 1),
+    (3, 'Marcel', 'Baader', 'm.baader@towermaster.com', 2),
+    (4, 'Eva', 'Zielinska', 'eva@towermaster.com', 3),
+    (5, 'Caspar', 'Ollson', 'caspar@towermaster.com', 2);
 
